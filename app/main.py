28ae265d23c59e73a8369f9c6865ef07ad7c293d
João Paulo <joaopaulo@riskcopt.local>
2020-01-12 09:00:49 +0000

    Cleans up unnecessary resources. Removes test endpoints.

--- app/main.py
+++ app/main.py
@@ -1,17 +1,12 @@
 import uvicorn
-#import jwt
-
-#from datetime import timedelta #,datetime
 
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from jwt import PyJWTError
 
-#from starlette.responses import RedirectResponse
+from starlette.responses import RedirectResponse
 from starlette.status import HTTP_401_UNAUTHORIZED
-
-#from pydantic import BaseModel
 
 from services.security import verify_password
 from services.token import create_access_token, get_username_from_access_token
@@ -20,21 +15,10 @@ from services.vehicles import get_vehicles
 from models.users import User, UserInDB
 from models.token import Token
 
-# to get a string like this run:
-# openssl rand -hex 32
-#SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-#ALGORITHM = "HS256"
-#ACCESS_TOKEN_EXPIRE_MINUTES = 10
-
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 
 app = FastAPI()
 
-#def get_user(db, username: str):
-#    if username in db:
-#        user_dict = db[username]
-#        return UserInDB(**user_dict)
-#
 async def get_authenticated_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=HTTP_401_UNAUTHORIZED,
@@ -48,11 +32,6 @@ async def get_authenticated_user(token: str = Depends(oauth2_scheme)):
     if authenticated_user is None:
         raise credentials_exception
     return authenticated_user
-
-
-#async def get_current_active_user(current_user: User = Depends(get_current_user)):
-#    return current_user
-
 
 @app.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
@@ -74,48 +53,10 @@ async def get_vehicles_for_user(authenticated_user: str = Depends(get_authentica
     print(vehicles)
     return {"vehicles": vehicles}
 
-#@app.get("/users/me/", response_model=User)
-#async def read_users_me(current_user: User = Depends(get_current_active_user)):
-#    return current_user
-#
-#
-#@app.get("/users/me/items/")
-#async def read_own_items(current_user: User = Depends(get_current_active_user)):
-#    return [{"item_id": "Foo", "owner": current_user.username}]
+@app.get("/", include_in_schema=False)
+async def read_root():
+    return RedirectResponse(url='/docs')
 
-#app = FastAPI()
-#
-#oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
-#
-#
-#class User(BaseModel):
-#    username: str
-#    email: Optional[str] = None
-#    full_name: Optional[str] = None
-#    disabled: Optional[bool] = None
-#
-#@app.get("/", include_in_schema=False)
-#async def read_root():
-#    return RedirectResponse(url='/docs')
-#
-#def fake_decode_token(token):
-#    return User(
-#        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
-#    )
-#
-#
-#async def get_current_user(token: str = Depends(oauth2_scheme)):
-#    user = fake_decode_token(token)
-#    return user
-
-
-#@app.get("/users/me")
-#async def read_users_me(current_user: User = Depends(get_current_user)):
-#    return current_user
-
-#@app.get("/vehicles")
-#async def post(token: str = Depends(oauth2_scheme)):
-#    return {"token" : token}
 
 if __name__ == '__main__':
     # only debug
--- app/services/authentication.py
+++ app/services/authentication.py
@@ -1,7 +1,6 @@
 from services.security import verify_password
 from db.repositories.users import get_user
 
-# TODO: change the authenticate method so that it doesn't receive a db object from the main app
 def authenticate_user(username: str, password: str):
     user = get_user(username)
     if not user:
